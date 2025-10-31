b805449c22e997a2a6050ae2b6089b1de26a6a46
Ivan Seryak <AmazingPain>
2025-10-31 20:50:01 +0300

    added animation and some scripts

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -2,24 +2,76 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public static PlayerBehavior Instance {  get; private set; }
 
     [SerializeField] private float movingSpeed = 5f;
-
+    private float minSpeed = 0.1f;
+    private bool isRunning = false;
 
     private Rigidbody2D rb;
-
+    
     private void Awake()
     {
+        Instance = this;
         rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void FixedUpdate()
     {
+        HandleMovement();
+    }
 
+    private void HandleMovement()
+    {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
         inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
 
+        if (Mathf.Abs(inputVector.x) > minSpeed || Mathf.Abs(inputVector.y)>minSpeed)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning= false;
+        }
+    }
+
+    public bool IsRunning()
+    {
+    
+
+        return isRunning;
+    }
+
+    public Vector3 GetPlayeScreenPosition()
+    {
+        Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        return playerScreenPosition;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/PlayerVisual.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour
+{
+    private Animator animatorPlayerVisual;
+    private SpriteRenderer spriteRenderer;
+    private const string IS_RUNNING = "IsRunning";
+
+    private void Awake()
+    {
+        animatorPlayerVisual = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        animatorPlayerVisual.SetBool(IS_RUNNING, PlayerBehavior.Instance.IsRunning());
+        AdjustPlayerFacingDirection();
+    }
+
+    private void AdjustPlayerFacingDirection ()
+    {
+        Vector3 mousePos =  GameInput.Instance.GetMousePosition();
+        Vector3 playerPosition = PlayerBehavior.Instance.GetPlayeScreenPosition();
+
+        if (mousePos.x < playerPosition.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
